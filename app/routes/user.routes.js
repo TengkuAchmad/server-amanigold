7e39fac607f365a70e9fc2f73411f9108dc730c2
= <=>
2024-05-02 20:42:06 +0700

    Add User Identity Function

--- app/routes/user.routes.js
+++ app/routes/user.routes.js
@@ -1,12 +1,16 @@
 // LIBRARY IMPORT
 const router = require("express").Router()
 
+// MIDDLEWARE IMPORT
+const authenticateToken = require("../middleware/middleware")
+
 // CONTROLLER IMPORT
 const user_controller = require("../controllers/user.controller")
 
 // ROUTER CONFIGURATION
-router.get("/user-management", user_controller.findOne);
+router.get("/user-management", authenticateToken, user_controller.findOne);
 router.post("/user-management/auth", user_controller.auth);
 router.post("/user-management", user_controller.signup);
 router.delete("/user-management", user_controller.deleteAll);
+
 module.exports = router
