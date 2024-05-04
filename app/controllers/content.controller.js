0238e31896639a9b765f8efe4619d1a79a3e7caf
= <=>
2024-05-05 00:37:11 +0700

    Test on File Handling

--- app/controllers/content.controller.js
+++ app/controllers/content.controller.js
@@ -12,24 +12,34 @@ const { SPACES_URL, SPACES_ACCESS_KEY, SPACES_SECRET_KEY }  = process.env
 const s3                = new AWS.S3({ endpoint: spacesEndpoint, accessKeyId: SPACES_ACCESS_KEY, secretAccessKey: SPACES_SECRET_KEY })
 
 exports.upload = async (req, res) => {
-    if (!req.files || !req.files.length) {
-        return res.status(400).json({ error: 'No file uploaded' });
-    }
+    try {
 
-    const file = req.files[0]
-
-    const params = {
-        Bucket: SPACES_URL,
-        Key: `content/content.${file.originalname.split('.').pop()}`,
-        Body: file.buffer,
-        ACL: 'public-read'
-    }
-
-    s3.upload(params, (err, data) => {
-        if (err) {
-            return res.status(500).json({ error: 'Failed to upload file' });
+        if (!req.files || !req.files.length) {
+            return res.status(400).json({ error: 'No file uploaded' });
         }
 
-        return res.json({ url: data.Location})
-    })
+        const file = req.files[0]
+
+        const timestamp = Date.now();
+        const randomString = Math.random().toString(36).substring(2);
+        const secureName = `${timestamp}-${randomString}.${file.originalname.split('.').pop()}`;
+        
+        
+        const params = {
+            Bucket: SPACES_URL,
+            Key: secureName,
+            Body: file.buffer,
+            ACL: 'public-read'
+        }
+        
+        s3.upload(params, (err, data) => {
+            if (err) {
+                return res.status(500).json({ error: 'Failed to upload file' + err });
+            }
+
+            return res.json({ url: data.Location})
+        })
+    } catch (error) {
+        return res.status(500).json({ error: error.message })
+    }
 }
