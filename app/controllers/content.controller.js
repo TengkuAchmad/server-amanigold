99e556c428acf02199808ee8534364ed3d98ab50
= <=>
2024-05-05 01:33:35 +0700

    Test on File Handling

--- app/controllers/content.controller.js
+++ app/controllers/content.controller.js
@@ -1,14 +1,25 @@
 // LIBRARY IMPORT
-const AWS       = require('aws-sdk')
-const multer    = require('multer')
-const upload    = multer()
+const { PutObjectCommand, S3Client } = require("@aws-sdk/client-s3")
+// const AWS       = require('aws-sdk')
+// const multer    = require('multer')
+// const upload    = multer()
 
 // CONSTANT IMPORT
 const { SPACES_URL, SPACES_ACCESS_KEY, SPACES_SECRET_KEY }  = process.env
 
 // AWS CONFIGURATION
-const spacesEndpoint    = new AWS.Endpoint(SPACES_URL)
-const s3                = new AWS.S3({ endpoint: spacesEndpoint, accessKeyId: SPACES_ACCESS_KEY, secretAccessKey: SPACES_SECRET_KEY })
+// const spacesEndpoint    = new AWS.Endpoint(SPACES_URL)
+// const s3                = new AWS.S3({ endpoint: spacesEndpoint, accessKeyId: SPACES_ACCESS_KEY, secretAccessKey: SPACES_SECRET_KEY })
+
+const s3Client          = new S3Client({
+    endpoint: SPACES_URL,
+    forcePathStyle: false,
+    region: 'sgp-1',
+    credentials: {
+        accessKeyId: SPACES_ACCESS_KEY,
+        secretAccessKey: SPACES_SECRET_KEY
+    }
+})
 
 exports.upload = async (req, res) => {
     try {
@@ -30,14 +41,18 @@ exports.upload = async (req, res) => {
             Body: file.buffer,
             ACL: 'public-read'
         }
-        
-        s3.upload(params, (err, data) => {
-            if (err) {
-                return res.status(500).json({ error: 'Failed to upload file' + err });
-            }
 
-            return res.json({ url: data.Location})
-        })
+        const data = await s3Client.send(new PutObjectCommand(params))
+
+        return res.json({ url: data.Location})
+        
+        // s3.upload(params, (err, data) => {
+        //     if (err) {
+        //         return res.status(500).json({ error: 'Failed to upload file' + err });
+        //     }
+
+        //     return res.json({ url: data.Location})
+        // })
     } catch (error) {
         return res.status(500).json({ error: error.message })
     }
