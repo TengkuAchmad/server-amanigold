947a3a6cb8eef040616787431b02ce2249a87e73
= <=>
2024-05-20 18:19:00 +0700

    Add Token Checker

--- app/routes/api.routes.js
+++ app/routes/api.routes.js
@@ -4,7 +4,10 @@ const router = require("express").Router()
 // CONTROLLER IMPORT
 const api_controller = require("../controllers/api.controller")
 
+// MIDDLEWARE IMPORT
+const { authenticateToken } = require("../middleware/middleware")
+
 // ROUTER CONFIGURATION
-router.get("/test", api_controller.test)
+router.get("/test", authenticateToken, api_controller.test)
 
 module.exports = router
