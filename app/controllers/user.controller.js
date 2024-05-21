f85f2f0c2802b4529e2c06dc59503b2f438e20db
= <=>
2024-05-22 02:54:52 +0700

    Add delete one user

--- app/controllers/user.controller.js
+++ app/controllers/user.controller.js
@@ -148,7 +148,7 @@ exports.deleteOne = async (req, res) => {
             }
         })
 
-        const availableCard = await prisma.cardData.findUnique({
+        const availableCard = await prisma.cardData.findMany({
             where: {
                 UUID_UA: uuid
             }
